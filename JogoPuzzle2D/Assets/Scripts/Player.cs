3529337d8ef4459d3e799bc44faf30aa8aed799c
Pedrito16 <proibidofantasma@gmail.com>
2025-03-07 23:41:44 -0300

    fiz o magnet te seguir quando for pra longe + corda
    ainda precisa ajustar muita coisa (fazer proxima vez: mecanica de jogar o magnet

--- JogoPuzzle2D/Assets/Scripts/Player.cs
+++ JogoPuzzle2D/Assets/Scripts/Player.cs
@@ -4,17 +4,69 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
-    [SerializeField] LineRenderer corda;
     [SerializeField] GameObject imã;
+    [SerializeField] float playerMoveSpeed;
+    [SerializeField] float maxDistanceToPullBack;
+    [SerializeField] bool haveMagnet;
+
+    [Header("Debug")]
+    [SerializeField] float distance;
+
+    //variaveis invisiveis
+    Rigidbody2D rb;
+    LineRenderer corda;
+    Rigidbody2D magnetRb;
+    SpringJoint2D magnetJoint;
     void Start()
     {
+        rb = GetComponent<Rigidbody2D>();
         corda = GetComponent<LineRenderer>();
+        magnetJoint = imã.GetComponent<SpringJoint2D>();
+        magnetRb = imã.GetComponent<Rigidbody2D>();
+        magnetJoint.distance = maxDistanceToPullBack;
     }
-
-    // Update is called once per frame
     void Update()
     {
+        float horizontal = Input.GetAxisRaw("Horizontal");
+        float vertical = Input.GetAxisRaw("Vertical");
+        Vector2 playerMovementVector = new Vector2(horizontal, vertical);
+        if(playerMovementVector != Vector2.zero)
+        {
+            playerMovementVector = playerMovementVector.normalized;
+        }
+        rb.velocity = playerMovementVector * playerMoveSpeed;
+        distance = Vector2.Distance(transform.position, imã.transform.position);
+
+        if (distance >= maxDistanceToPullBack)
+        {
+            magnetJoint.enabled = true;
+            magnetRb.angularDrag = 0.1f;
+            magnetRb.drag = 0.1f;
+        }
+        else
+        {
+            magnetJoint.enabled = false;
+            magnetRb.angularDrag = 5;
+            magnetRb.drag = 5;
+        }
         corda.SetPosition(0, transform.position);
         corda.SetPosition(1, imã.transform.position);
+    }
+    void ThrowMagnet()
+    {
+
+    }
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if(collision.tag == "Magnet") //to usando com tag pois: não sei usar interface "direito" e porque magnet não tem script para aplicar interface :P
+        {
+            imã.SetActive(false);
+            corda.enabled = false;
+            haveMagnet = true;
+        }
+    }
+    private void OnDrawGizmos()
+    {
+        Gizmos.DrawWireSphere(imã.transform.position, maxDistanceToPullBack);
     }
 }

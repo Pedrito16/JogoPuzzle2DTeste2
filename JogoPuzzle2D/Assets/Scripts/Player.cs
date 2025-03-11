0983b5d80da66b326ef6b6c391b832dc82a6f7db
Pedrito16 <pedrogabrielvicente98@gmail.com>
2025-03-11 16:38:39 -0300

    to arrumando a jogada

--- JogoPuzzle2D/Assets/Scripts/Player.cs
+++ JogoPuzzle2D/Assets/Scripts/Player.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 public enum PlayerState
 {
@@ -17,15 +18,17 @@ public class Player : MonoBehaviour
     [Header("Debug")]
     [SerializeField] float tempoSegurado;
     [SerializeField] public static PlayerState state;
-    [SerializeField] bool haveMagnet;
+    [SerializeField] public bool hasMagnet;
     [SerializeField] bool canMove;
 
     //variaveis invisiveis
     Rigidbody2D rb;
+    Rigidbody2D magnetRb;
     LineRenderer corda;
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
+        magnetRb = magnet.GetComponent<Rigidbody2D>();
         corda = GetComponent<LineRenderer>();
         state = PlayerState.notPulling;
         canMove = true;
@@ -40,17 +43,22 @@ public class Player : MonoBehaviour
             playerMovementVector = playerMovementVector.normalized;
         }
         if(canMove)
-        {
             rb.velocity = playerMovementVector * playerMoveSpeed;
-        }
         else
-        {
             rb.velocity = Vector2.zero;
+
+        if (Input.GetButton("Fire1") && tempoSegurado < 3)
+        {
+            tempoSegurado += Time.deltaTime;
+        }
+        if (Input.GetButtonDown("Fire1"))
+        {
+            ThrowMagnet(tempoSegurado);
         }
     }
     private void FixedUpdate()
     {
-        if (Input.GetButton("Jump") && !haveMagnet)
+        if (Input.GetButton("Jump") && !hasMagnet)
         {
             canMove = false;
             StartCoroutine(pullMagnet());
@@ -62,10 +70,33 @@ public class Player : MonoBehaviour
             StopAllCoroutines();
             canMove = true;
         }
+        if (Input.GetButton("Fire1") && tempoSegurado < 3)
+        {
+            tempoSegurado += Time.deltaTime;
+        }
+        if (Input.GetButtonDown("Fire1"))
+        {
+            ThrowMagnet(tempoSegurado);
+        }
     }
-    void ThrowMagnet()
+    void ThrowMagnet(float força)
     {
+        print("Ativou ThrowMagnet");
+        hasMagnet = false;
+        Vector3 mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        magnet.SetActive(true);
+        magnet.transform.position = transform.position;
+        magnet.transform.rotation = Quaternion.Euler(new Vector3(magnet.transform.rotation.x, magnet.transform.rotation.y, GetPlayerLookingPosition(mousePos)));
 
+        magnetRb.AddForce(mousePos * força);
+        corda.enabled = true;
+    }
+    float GetPlayerLookingPosition(Vector3 mousePos)
+    {
+        Vector3 direction = transform.position - mousePos;
+        float calculo = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg;
+
+        return calculo + 90;
     }
     IEnumerator pullMagnet()
     {
@@ -83,7 +114,7 @@ public class Player : MonoBehaviour
         {
             magnet.SetActive(false);
             corda.enabled = false;
-            haveMagnet = true;
+            hasMagnet = true;
         }
     }
 }
--- JogoPuzzle2D/Assets/Scripts/RopeSystem.cs
+++ JogoPuzzle2D/Assets/Scripts/RopeSystem.cs
@@ -7,23 +7,28 @@ public class RopeSystem : MonoBehaviour
     [SerializeField] Transform[] pontosCorda;
     [SerializeField] Transform magnet;
     [SerializeField] LineRenderer corda;
-    [SerializeField] Transform player;
+    [SerializeField] Transform playerTransform;
+    [SerializeField] Player player;
     void Start()
     {
-        corda = player.gameObject.GetComponent<LineRenderer>();
+        player = playerTransform.gameObject.GetComponent<Player>();
+        corda = playerTransform.gameObject.GetComponent<LineRenderer>();
         corda.positionCount = 8;
     }
 
     void Update()
     {
         //precisa colocar tudo isso em um for, mesmo que seja nao recomendado
-        corda.SetPosition(0, player.position);
-        corda.SetPosition(1, pontosCorda[0].position);
-        corda.SetPosition(2, pontosCorda[1].position);
-        corda.SetPosition(3, pontosCorda[2].position);
-        corda.SetPosition(4, pontosCorda[3].position);
-        corda.SetPosition(5, pontosCorda[4].position);
-        corda.SetPosition(6, pontosCorda[5].position);
-        corda.SetPosition(7, magnet.position);
+        if (!player.hasMagnet)
+        {
+            corda.SetPosition(0, playerTransform.position);
+            corda.SetPosition(1, pontosCorda[0].position);
+            corda.SetPosition(2, pontosCorda[1].position);
+            corda.SetPosition(3, pontosCorda[2].position);
+            corda.SetPosition(4, pontosCorda[3].position);
+            corda.SetPosition(5, pontosCorda[4].position);
+            corda.SetPosition(6, pontosCorda[5].position);
+            corda.SetPosition(7, magnet.position);
+        }
     }
 }
